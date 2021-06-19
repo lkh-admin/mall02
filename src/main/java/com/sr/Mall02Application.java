fad29eeee6ac1237ea656693ada89903d74216ff
kaihel <895421405@qq.com>
2021-06-19 11:18:59 +0800

    首次提交

---
+++ src/main/java/com/sr/Mall02Application.java
@@ -0,0 +1,13 @@
+package com.sr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall02Application.class, args);
+    }
+
+}
